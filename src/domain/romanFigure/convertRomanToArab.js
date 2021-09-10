b2f449bf2aa9e7327c1dd77d5aaebbb2c22fe05c
J-HOVELAQUE <j.hovelaque@orange.fr>
2021-09-10 17:11:43 +0200

    function for convert roman figure in arab figure

---
+++ src/domain/romanFigure/convertRomanToArab.js
@@ -0,0 +1,74 @@
+function convertRomanToArab(romanValue) {
+  const figureList = romanValue.split("");
+
+  return figureList.reduce((absoluteValue, currentFigure, index) => {
+    switch (currentFigure) {
+      case "I":
+        if (figureList[index + 1] === "V" || figureList[index + 1] === "X") {
+          return absoluteValue;
+        }
+        absoluteValue += 1;
+        return absoluteValue;
+
+      case "V":
+        if (figureList[index - 1] !== "I") {
+          absoluteValue += 5;
+        } else {
+          absoluteValue += 4;
+        }
+        return absoluteValue;
+
+      case "X":
+        if (figureList[index - 1] === "I") {
+          absoluteValue += 9;
+          return absoluteValue;
+        }
+        if (figureList[index + 1] === "L" || figureList[index + 1] === "C") {
+          return absoluteValue;
+        }
+        absoluteValue += 10;
+        return absoluteValue;
+
+      case "L":
+        if (figureList[index - 1] !== "X") {
+          absoluteValue += 50;
+        } else {
+          absoluteValue += 40;
+        }
+        return absoluteValue;
+
+      case "C":
+        if (figureList[index - 1] === "X") {
+          absoluteValue += 90;
+          return absoluteValue;
+        }
+        if (figureList[index + 1] === "D" || figureList[index + 1] === "M") {
+          return absoluteValue;
+        }
+        absoluteValue += 100;
+        return absoluteValue;
+
+      case "D":
+        if (figureList[index - 1] !== "C") {
+          absoluteValue += 500;
+        } else {
+          absoluteValue += 400;
+        }
+        return absoluteValue;
+
+      case "M":
+        if (figureList[index - 1] === "C") {
+          absoluteValue += 900;
+          return absoluteValue;
+        }
+
+        absoluteValue += 1000;
+        return absoluteValue;
+
+      default:
+        return absoluteValue;
+    }
+  }, 0);
+}
+
+module.exports = convertRomanToArab;
---
+++ src/domain/romanFigure/convertRomanToArab.test.js
@@ -0,0 +1,75 @@
+const convertRomanToArab = require("./convertRomanToArab");
+
+describe("Testing function for converting roman value to arab value", () => {
+  describe("Given that I wish to convert 'III' to arab value", () => {
+    describe("When I call the function convertRomanToArab with 'III' as args", () => {
+      it("Then I receive 3", () => {
+        expect(convertRomanToArab("III")).toBe(3);
+      });
+    });
+  });
+
+  describe("Given that I wish to convert 'V' to arab value", () => {
+    describe("When I call the function convertRomanToArab with 'III' as args", () => {
+      it("Then I receive 5", () => {
+        expect(convertRomanToArab("V")).toBe(5);
+      });
+    });
+  });
+
+  describe("Given that I wish to convert 'X' to arab value", () => {
+    describe("When I call the function convertRomanToArab with 'X' as args", () => {
+      it("Then I receive 10", () => {
+        expect(convertRomanToArab("X")).toBe(10);
+      });
+    });
+  });
+
+  describe("Given that I wish to convert 'IV' to arab value", () => {
+    describe("When I call the function convertRomanToArab with 'IV' as args", () => {
+      it("Then I receive 4", () => {
+        expect(convertRomanToArab("IV")).toBe(4);
+      });
+    });
+  });
+
+  describe("Given that I wish to convert 'IX' to arab value", () => {
+    describe("When I call the function convertRomanToArab with 'IX' as args", () => {
+      it("Then I receive 9", () => {
+        expect(convertRomanToArab("IX")).toBe(9);
+      });
+    });
+  });
+
+  describe("Given that I wish to convert 'XIV' to arab value", () => {
+    describe("When I call the function convertRomanToArab with 'XIV' as args", () => {
+      it("Then I receive 14", () => {
+        expect(convertRomanToArab("XIV")).toBe(14);
+      });
+    });
+  });
+
+  describe("Given that I wish to convert 'XLIV' to arab value", () => {
+    describe("When I call the function convertRomanToArab with 'XLIV' as args", () => {
+      it("Then I receive 44", () => {
+        expect(convertRomanToArab("XLIV")).toBe(44);
+      });
+    });
+  });
+
+  describe("Given that I wish to convert 'CMXCIII' to arab value", () => {
+    describe("When I call the function convertRomanToArab with 'CMXCIII' as args", () => {
+      it("Then I receive 993", () => {
+        expect(convertRomanToArab("CMXCIII")).toBe(993);
+      });
+    });
+  });
+
+  describe("Given that I wish to convert 'MMCDLIV' to arab value", () => {
+    describe("When I call the function convertRomanToArab with 'MMCDLIV' as args", () => {
+      it("Then I receive 2454", () => {
+        expect(convertRomanToArab("MMCDLIV")).toBe(2454);
+      });
+    });
+  });
+});
